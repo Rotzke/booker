ab8b447eafe71f3f556eb981a59c9bdfc9f24b61
Nikita Rotsky <kurt.rotzke@gmail.com>
2019-02-13 01:00:08 +0200

    Added basic templates, models and views.

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:master_id>/', views.detail, name='detail'),
+    path('booking/<int:master_id>', views.detail, name='detail'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -6,8 +6,7 @@ from booking.models import Master
 
 def index(request):
     masters_list = Master.objects.order_by('-id')
-    output = '<br>'.join([f'<a href="{q.id}">{q.get_full_name}</a>' for q in masters_list])
-    return HttpResponse(output)
+    return render(request, 'booking/index.html', {'masters': masters_list})
 
 
 def detail(request, master_id):
